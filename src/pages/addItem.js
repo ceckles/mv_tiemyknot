f033a646843abe8e4f3b2637acd704d0c10aafbb
CrystalMorris <l.seagypsy.l@gmail.com>
2021-09-14 17:43:12 -0500

    added some css to addItem page

--- src/pages/addItem.js
+++ src/pages/addItem.js
@@ -7,7 +7,29 @@ import SaveIcon from '@material-ui/icons/Save';
 import TextField from '@material-ui/core/TextField';
 
 //Styles
-const useStyles = makeStyles((theme) => ({  }));
+const useStyles = makeStyles((theme) => ({ 
+    root: {
+        background: 'linear-gradient(45deg, #9013FE 15%, #50E3C2 90%)',
+        width: '100vw',
+        minHeight: '100vh',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: 'center'
+        
+      },
+    textbox:{
+        
+        width: '60vw',
+    },
+    button: {
+        width: "max-content",
+        display: "flex",
+        flexDirection: "column",
+        
+        justifyContent: "center",
+    }
+ }));
 
 const AddItem = () =>{
         //use styles
@@ -17,21 +39,14 @@ const AddItem = () =>{
             <div><p>Add Item</p>  
                 
                 <FormControl className={classes.root} noValidate autoComplete="off">                   
-                    <TextField required id="outlined-required" label="Item Name" variant="outlined" /><br/>
-                    <TextField required id="outlined-required"label="Item Link" variant="outlined" /><br/>
-                </FormControl><br/>
+                    <TextField className={classes.textbox} required id="outlined-required" label="Item Name" variant="outlined" /><br/>
+                    <TextField className={classes.textbox} required id="outlined-required"label="Item Link" variant="outlined" /><br/>
+                
                
     
-                <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Save
-      </Button>
-                
+                <Button variant="contained" color="primary" size="large" className={classes.button}
+        startIcon={<SaveIcon />} > Save     </Button>
+                </FormControl><br/>
 
 
             </div>);
