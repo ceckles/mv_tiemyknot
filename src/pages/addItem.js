a37ef458f2cf99b94df0d20789ff314b7a6c0cfc
CrystalMorris <l.seagypsy.l@gmail.com>
2021-09-14 16:48:31 -0500

    initial form for add item

--- src/pages/addItem.js
+++ src/pages/addItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
+import FormControl from '@material-ui/core/FormControl';
+import { InputLabel, Input } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import TextField from '@material-ui/core/TextField';
 
 //Styles
 const useStyles = makeStyles((theme) => ({  }));
@@ -9,7 +14,26 @@ const AddItem = () =>{
         const classes = useStyles();
 
         return(
-            <div><p>Add Item</p>
+            <div><p>Add Item</p>  
+                
+                <FormControl className={classes.root} noValidate autoComplete="off">                   
+                    <TextField required id="outlined-required" label="Item Name" variant="outlined" /><br/>
+                    <TextField required id="outlined-required"label="Item Link" variant="outlined" /><br/>
+                </FormControl><br/>
+               
+    
+                <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<SaveIcon />}
+      >
+        Save
+      </Button>
+                
+
+
             </div>);
 };
 
