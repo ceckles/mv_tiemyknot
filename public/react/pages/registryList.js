10aef7a283f4da46574935011af2921b1dc9e786
Chad Eckles <chad1020@gmail.com>
2021-09-21 16:22:24 -0500

    Center and Style the reglist and route for Nate Add Reg

--- public/react/pages/registryList.js
+++ public/react/pages/registryList.js
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { makeStyles, theme } from "@material-ui/core/styles";
 
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Box from "@material-ui/core/Box";
 const useStyles = makeStyles((theme) => ({
-	root: {
-		minWidth: '100%',
-		minHeight: '100vh',
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-	  },
+  root: {
+    minWidth: "100%",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
 }));
 
 const RegistryList = (props) => {
-	  //use styles
-	  const classes = useStyles();
- 
-	return(
-		<div className={classes.root} align="center">
-		<br/><br/><br/><br/>
-		{
-			
+	const [regID, setRegID] = useState(0);
+  //use styles
+  const classes = useStyles();
+  return (
+    <div className={classes.root} align="center">
+	  <Box>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper"}} align="center">
+		  {
 			props.registryData.map((registry, idx) => {
-				console.log(registry);
-				return(<div key={idx}><strong>{registry.groomName}, {registry.brideName}</strong></div>)
+			   return (
+					<ListItem key={idx}>
+					<ListItemText primary={registry.brideName} secondary={registry.groomName} key={idx}/>
+					</ListItem>
+			   );
 			})
-		}
-		</div>
-	)
-}
+		   }
+
+      </List>
+	  </Box>
+    </div>
+  );
+};
 export default RegistryList;
